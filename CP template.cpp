0a6a5f083d2398c7a81bebafeb2701d0566fd140
AFRIDI HAQUE <85033515+Afridi-haque7@users.noreply.github.com>
2022-10-20 20:03:05 +0530

    Add files via upload

---
+++ CP template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define forn(n) for(int i = 0; i < n; i++)
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		
+	}
+	return 0;
+}
