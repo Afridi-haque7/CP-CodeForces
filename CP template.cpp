31172dabc69c1ee7bc3f5468648022f3c8484dab
AFRIDI HAQUE <85033515+Afridi-haque7@users.noreply.github.com>
2022-10-27 15:09:22 +0530

    Update CP template.cpp

--- CP template.cpp
+++ CP template.cpp
@@ -1,13 +1,44 @@
-#include<bits/stdc++.h>
-#define ll long long
-#define forn(n) for(int i = 0; i < n; i++)
+//	Headers
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pdbs;
 using namespace std;
 
+//	Special Numbers
+const mod 1000000007
+const inf 1e18
+
+//	Datatypes
+#define ll long long
+#define dll long long double
+
+//	Vector operations
+#define v(i) vector<int>
+#define pb push_back
+#define vb v.begin()
+#define ve v.end()
+
+//	Pair operations
+#define pll pair<long, long>
+#define pii pair<int, int>
+#define f first
+#define s second
+#define mp make_pair
+
+//	Loops
+#define w(t) int t; cin >> t; while(t--)
+#define f(i,a,b) for(int i = a; i < b; i++)
+#define f(i,a,b) for(int i = a; i >= b; i--)
+
+//	Printing Outputs
+#define py cout<<"YES"<<endl;
+#define pn cout<<"NO"<<endl;
+#define inp int n; cin >> n;
+
+//	Driver Function Start
+//	Type Code from here
 int main(){
-	int t;
-	cin >> t;
-	while(t--){
-		
-	}
+	
 	return 0;
 }
